80034453b878c1d9131f7fe52d7ccfb9570ebf07
Rafael <rafael.glez@gmail.com>
2024-08-23 18:51:18 -0600

    Initial Commit

---
+++ Autolayout_SUI/Autolayout_SUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Autolayout_SUIApp.swift
+//  Autolayout_SUI
+//
+//  Created by Rafael Gonzalez on 23/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Autolayout_SUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
